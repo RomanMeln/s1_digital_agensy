21e41db676d6e8d1bbc3574013f6456f9fc76471
RomanMeln <tupozarabotok@gmail.com>
2026-05-15 17:37:01 +0300

    Создал базовый шаблон. Сделал наследование на каждую страницу. Для каждой страницы вынес стили в отдельный файл.

--- s1_digital_agensy/mainapp/views.py
+++ s1_digital_agensy/mainapp/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainapp/index.html')
 
-
 def products(request):
     return render(request, 'mainapp/projects.html')
-
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
