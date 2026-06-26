f5bdfb2b206f1784288d35d6a0a6c599f5bf2407
RomanMeln <tupozarabotok@gmail.com>
2026-06-26 20:36:02 +0300

    Сделал отображение данных статей в карточках на странице блог. Во views.py сделал проверку пустого запроса при выборке статей из базы данных. Сделал простое отображение всех доступных категорий для будущего фильтра. Поправил blog.css для переноса категорий на новую строку.

--- s1_digital_agensy/blogapp/views.py
+++ s1_digital_agensy/blogapp/views.py
@@ -4,4 +4,20 @@ from .models import Post, CategoryName
 
 def blog(request):
     posts = Post.objects.all().order_by('-created_at')[:4]
-    return render(request, 'blogapp/blog.html', {'posts': posts})
+    categories = CategoryName.objects.all()
+    # проверка наличия статьи
+    if posts:
+        default_post = posts[0] # дефолтный пост
+    else:
+        # если постов нет
+        default_post = Post(
+            title="Заголовок новой статьи",
+            text="Текст появится, когда вы добавите статьи в админ-панель."
+            # description и image подставятся автоматически из дефолтов модели!
+        )
+
+        # Добавляем его в список, чтобы posts.0 на главной странице не вызвал ошибку
+        posts.append(default_post)
+
+    return render(request, 'blogapp/blog.html', {'posts': posts,
+                                                 'def_post': default_post, 'categories': categories})
