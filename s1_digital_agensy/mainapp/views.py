51c10c297dd08e4c31c025c11ed8e10a25a665a9
RomanMeln <tupozarabotok@gmail.com>
2026-07-01 23:41:41 +0300

    На главной странице сделал отображение данных проектов в карточках секции "Наши проекты" и статей блога в карточках секции "Блог". Во views.py добавил проверку пустого запроса при выборке проектов и постов из базы данных. Изменил код главной страницы и код CSS. Удалил ненужный код.

--- s1_digital_agensy/mainapp/views.py
+++ s1_digital_agensy/mainapp/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
 from .models import ProjectStart
+from blogapp.models import Post, CategoryName
+from projectapp.models import Project
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    projects = Project.objects.all().order_by('-created_at')[:5]
 
-def products(request):
-    return render(request, 'mainapp/projects.html')
+    if projects:
+        default_project = projects[0] # дефолтный проект
+    else:
+        # если проектов нет
+        default_project = Project(
+            title="Тут должно быть название проекта",
+            text="Тут должно все о проекте"
+            # description и image подставятся автоматически из дефолтов модели!
+        )
+
+        # Добавляем его в список, чтобы posts.0 на главной странице не вызвал ошибку
+        projects.append(default_project)
+
+    posts = Post.objects.all().order_by('-created_at')[:5]
+    # проверка наличия статьи
+    if posts:
+        default_post = posts[0]  # дефолтный пост
+    else:
+        # если постов нет
+        default_post = Post(
+            title="Заголовок новой статьи",
+            text="Текст появится, когда вы добавите статьи в админ-панель."
+            # description и image подставятся автоматически из дефолтов модели!
+        )
+
+        # Добавляем его в список, чтобы posts.0 на главной странице не вызвал ошибку
+        posts.append(default_post)
+
+    return render(request, 'mainapp/index.html', {'posts': posts, 'def_post': default_post,
+                                                  'projects': projects, 'def_project': default_project})
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
 
 def about_us(request):
     return render(request, 'mainapp/about_us.html')
-
-# def blog(request):
-#     return render(request, 'blogapp/blog.html')
 
 def services(request):
     return render(request, 'mainapp/services.html')
